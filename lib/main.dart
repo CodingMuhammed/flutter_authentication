684b3ff00d81e212c5106113f227be757455960c
Muhammed Hazem <muhammedhazem2099@gmail.com>
2022-06-25 17:11:18 +0300

    Adding

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<void> main() async{
+  await Firebase.initializeApp();
+  WidgetsFlutterBinding().ensureVisualUpdate();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ,
+    );
+  }
+}
+
